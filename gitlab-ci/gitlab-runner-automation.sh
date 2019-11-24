e8e201f136918f944e4d33c9cd97acc740690b13
Pavel Korzhenko <pksmall@live.ru>
2019-11-24 14:22:54 -0500

    add build reddit

---
+++ gitlab-ci/gitlab-runner-automation.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+docker run --rm -v /srv/gitlab-runner/config:/etc/gitlab-runner gitlab/gitlab-runner register \
+  --non-interactive \
+  --executor "docker" \
+  --docker-image alpine:latest \
+  --url "https://gitlab.com/" \
+  --registration-token "PROJECT_REGISTRATION_TOKEN" \
+  --description "docker-runner" \
+  --tag-list "docker,aws" \
+  --run-untagged="true" \
+  --locked="false" \
+  --access-level="not_protected"
